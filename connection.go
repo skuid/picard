a2a4c067e7cdf7f7d7208236f7cd86c3d95bc2f6
Brian Newton <brian.newton@skuid.com>
2018-09-26 15:22:33 -0400

    PLIN-1804: Adding a traced connection option to picard\'s open

--- connection.go
+++ connection.go
@@ -2,6 +2,9 @@ package picard
 
 import (
 	"database/sql"
+
+	"github.com/lib/pq"
+	sqltrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/database/sql"
 )
 
 var conn *sql.DB
@@ -9,6 +12,23 @@ var conn *sql.DB
 // CreateConnection creates a database connection using the provided arguments
 func CreateConnection(connstr string) error {
 	db, err := sql.Open("postgres", connstr)
+
+	if err != nil {
+		return err
+	}
+
+	if err = db.Ping(); err != nil {
+		return err
+	}
+
+	conn = db
+	return nil
+}
+
+// CreateTracedConnection creates a database connection using the provided arguments
+func CreateTracedConnection(connstr string) error {
+	sqltrace.Register("postgres", &pq.Driver{})
+	db, err := sqltrace.Open("postgres", connstr)
 
 	if err != nil {
 		return err
