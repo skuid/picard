9a3a5a10c9b8ea23a7c61aa7ac2c06519a18a5d4
Brian Newton <brian.newton@skuid.com>
2019-05-02 09:29:28 -0400

    Making the build func tests easier to understand

--- query/build_test.go
+++ query/build_test.go
@@ -10,10 +10,11 @@ import (
 func TestQueryBuilder(t *testing.T) {
 	orgID := "00000000-0000-0000-0000-000000000001"
 	testCases := []struct {
-		desc     string
-		model    interface{}
-		assoc    []tags.Association
-		expected *Table
+		desc         string
+		model        interface{}
+		assoc        []tags.Association
+		expected     string
+		expectedArgs []interface{}
 	}{
 		{
 			"should return a single table with a few columns",
@@ -21,26 +22,18 @@ func TestQueryBuilder(t *testing.T) {
 				Name: "pops",
 			},
 			nil,
-			&Table{
-				Counter: 1,
-				Alias:   "t0",
-				Name:    "parentmodel",
-				columns: []string{
-					"id",
-					"organization_id",
-					"name",
-					"parent_id",
-				},
-				Wheres: []Where{
-					Where{
-						Field: "organization_id",
-						Val:   "00000000-0000-0000-0000-000000000001",
-					},
-					Where{
-						Field: "name",
-						Val:   "pops",
-					},
-				},
+			fmtSQL(`
+				SELECT
+					t0.id AS "t0.id",
+					t0.organization_id AS "t0.organization_id",
+					t0.name AS "t0.name",
+					t0.parent_id AS "t0.parent_id"
+				FROM parentmodel AS t0
+				WHERE t0.organization_id = $1 AND t0.name = $2
+			`),
+			[]interface{}{
+				orgID,
+				"pops",
 			},
 		},
 		{
@@ -53,48 +46,25 @@ func TestQueryBuilder(t *testing.T) {
 					Name: "ReferenceTo",
 				},
 			},
-			&Table{
-				Counter: 2,
-				Alias:   "t0",
-				Name:    "field",
-				columns: []string{
-					"id",
-					"organization_id",
-					"name",
-					"object_id",
-					"reference_id",
-				},
-				Joins: []Join{
-					{
-						Type:        "left",
-						ParentField: "t0.reference_id",
-						JoinField:   "t1.id",
-						Table: &Table{
-							Alias: "t1",
-							Name:  "reference_to",
-							columns: []string{
-								"id",
-								"organization_id",
-							},
-							Wheres: []Where{
-								{
-									Field: "organization_id",
-									Val:   "00000000-0000-0000-0000-000000000001",
-								},
-							},
-						},
-					},
-				},
-				Wheres: []Where{
-					Where{
-						Field: "organization_id",
-						Val:   "00000000-0000-0000-0000-000000000001",
-					},
-					Where{
-						Field: "name",
-						Val:   "a_field",
-					},
-				},
+			fmtSQL(`
+				SELECT
+					t0.id AS "t0.id",
+					t0.organization_id AS "t0.organization_id",
+					t0.name AS "t0.name",
+					t0.object_id AS "t0.object_id",
+					t0.reference_id AS "t0.reference_id",
+					t1.id AS "t1.id",
+					t1.organization_id AS "t1.organization_id"
+				FROM field AS t0
+				LEFT JOIN reference_to AS t1 ON t1.id = t0.reference_id
+				WHERE
+					t0.organization_id = $1 AND
+					t0.name = $2 AND t1.organization_id = $3
+			`),
+			[]interface{}{
+				orgID,
+				"a_field",
+				orgID,
 			},
 		},
 	}
@@ -103,26 +73,31 @@ func TestQueryBuilder(t *testing.T) {
 		t.Run(tc.desc, func(t *testing.T) {
 			assert := assert.New(t)
 
-			actual, err := Build(orgID, tc.model, tc.assoc)
-
+			tbl, err := Build(orgID, tc.model, tc.assoc)
 			assert.NoError(err)
-			assert.Equal(tc.expected.Counter, actual.Counter)
-			assert.Equal(tc.expected.Alias, actual.Alias)
-			assert.Equal(tc.expected.Name, actual.Name)
 
-			for i, actualJoin := range actual.Joins {
-				expectedJoin := tc.expected.Joins[i]
-				assert.Equal(expectedJoin.Type, actualJoin.Type)
-				assert.Equal(expectedJoin.ParentField, actualJoin.ParentField)
-				assert.Equal(expectedJoin.JoinField, actualJoin.JoinField)
+			actual, actualArgs, err := tbl.ToSQL()
+			assert.NoError(err)
+			assert.Equal(tc.expected, actual)
+			assert.Equal(tc.expectedArgs, actualArgs)
 
-				actualJTbl := *actualJoin.Table
-				expectedJTbl := *expectedJoin.Table
+			// assert.Equal(tc.expected.Counter, actual.Counter)
+			// assert.Equal(tc.expected.Alias, actual.Alias)
+			// assert.Equal(tc.expected.Name, actual.Name)
 
-				assert.Equal(expectedJTbl, actualJTbl)
-			}
+			// for i, actualJoin := range actual.Joins {
+			// 	expectedJoin := tc.expected.Joins[i]
+			// 	assert.Equal(expectedJoin.Type, actualJoin.Type)
+			// 	assert.Equal(expectedJoin.ParentField, actualJoin.ParentField)
+			// 	assert.Equal(expectedJoin.JoinField, actualJoin.JoinField)
 
-			assert.Equal(tc.expected.Wheres, actual.Wheres)
+			// 	actualJTbl := *actualJoin.Table
+			// 	expectedJTbl := *expectedJoin.Table
+
+			// 	assert.Equal(expectedJTbl, actualJTbl)
+			// }
+
+			// assert.Equal(tc.expected.Wheres, actual.Wheres)
 
 		})
 	}
